86abcc34730aa73b7c337d801fe8e78364a62dd9
Todd Hiestand <todd@343design.com>
2013-12-23 23:11:40 -0500

    Removing sass :(

--- head.php
+++ head.php
@@ -8,13 +8,12 @@
 	
 		<div class="row">
 	
-			<div class="siteid col span_8">
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<!-- Comment out the site description unless we want it <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2> -->
+			<div class="siteid col span_10">
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>		
 			</div>
 	
-			<div class="nav col span_16">
-				<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+			<div class="nav col span_14">
+				<?php wp_nav_menu( array( 'theme_location' => 'utility-menu' ) ); ?>
 				<!-- The href attribute is used to load external HTML content into your panel -->
 			</div>
 	
