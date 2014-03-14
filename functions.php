ceda7735ad4d749ed2cad85dbc0245ed8a17c03f
Todd Hiestand <todd@purecharity.com>
2014-03-14 07:30:36 -0400

    Custom User fields

--- functions.php
+++ functions.php
@@ -116,7 +116,6 @@ add_action('login_head', 'custom_login');
        echo $output;
    }
 
- 
  // Add some featured images and sizes
  
  if ( function_exists( 'add_theme_support' ) ) { 
@@ -137,6 +136,60 @@ add_action('login_head', 'custom_login');
          echo '| <a href="'.get_bloginfo('wpurl').'/wp-admin/comment.php?action=cdc&dt=spam&c='.$id.'">spam</a>';
      }
  }
+
+ // remove dumb admin fields
+
+function add_twitter_contactmethod( $contactmethods ) {
+  unset($contactmethods['aim']);
+  unset($contactmethods['jabber']);
+  unset($contactmethods['yim']);
+  return $contactmethods;
+}
+add_filter('user_contactmethods','add_twitter_contactmethod',10,1);
+
+/**
+ * Add additional custom field
+ */
+
+add_action ( 'show_user_profile', 'my_show_extra_profile_fields' );
+add_action ( 'edit_user_profile', 'my_show_extra_profile_fields' );
+
+function my_show_extra_profile_fields ( $user )
+{
+?>
+    <h3>Extra profile information</h3>
+    <table class="form-table">
+        <tr>
+            <th><label for="twitter">Twitter</label></th>
+            <td>
+                <input type="text" name="twitter" id="twitter" value="<?php echo esc_attr( get_the_author_meta( 'twitter', $user->ID ) ); ?>" class="regular-text" /><br />
+                <span class="description">Please enter your Twitter username.</span>
+            </td>
+        </tr>
+        <tr>
+            <th><label for="facebook">Facebook</label></th>
+            <td>
+                <input type="text" name="facebook" id="twitter" value="<?php echo esc_attr( get_the_author_meta( 'facebook', $user->ID ) ); ?>" class="regular-text" /><br />
+                <span class="description">Please add the link to your Facebook page.</span>
+            </td>
+        </tr>
+    </table>
+<?php
+}
+
+add_action ( 'personal_options_update', 'my_save_extra_profile_fields' );
+add_action ( 'edit_user_profile_update', 'my_save_extra_profile_fields' );
+
+function my_save_extra_profile_fields( $user_id )
+{
+    if ( !current_user_can( 'edit_user', $user_id ) )
+        return false;
+    /* Copy and paste this line for additional fields. Make sure to change 'twitter' to the field ID. */
+    update_usermeta( $user_id, 'twitter', $_POST['twitter'] );
+    update_usermeta( $user_id, 'facebook', $_POST['facebook'] );
+}
+
+
  
  // Get rid of those self-pings
  function no_self_ping( &$links ) {
