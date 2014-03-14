49ffdafbf51f40fb3296d43b615cb399ed12bcb7
Todd Hiestand <todd@purecharity.com>
2014-03-14 07:32:11 -0400

    subnav function

--- functions.php
+++ functions.php
@@ -334,5 +334,24 @@ if ( is_user_logged_in() ) {
   add_action('wp_head', 'gist_custom_admin_styles'); 
 }
 
+// LETS CREAT A FUNCTIN FOR THE SUBNAV
+if(!function_exists('get_post_top_ancestor_id')){
+/**
+ * Gets the id of the topmost ancestor of the current page. Returns the current
+ * page's id if there is no parent.
+ *
+ * @uses object $post
+ * @return int
+ */
+function get_post_top_ancestor_id(){
+    global $post;
+
+    if($post->post_parent){
+        $ancestors = array_reverse(get_post_ancestors($post->ID));
+        return $ancestors[0];
+    }
+
+    return $post->ID;
+}}
 
 ?>
