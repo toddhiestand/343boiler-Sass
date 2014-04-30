ea0a5bbe85a250d9d870c7ee1bc3566277d29ce8
Todd Hiestand <todd@purecharity.com>
2014-04-30 12:13:53 -0500

    Add open graph to header

--- header.php
+++ header.php
@@ -21,4 +21,24 @@
 		<!-- This is the theme's responsive framework stylesheet, delete it at your own peril! -->
 		<link href="<?php bloginfo('template_directory'); ?>/css/responsive-gs-24col.css" rel="stylesheet" type="text/css" />
 
+		<!-- add open graph -->
+		<?php if (have_posts()):while(have_posts()):the_post(); endwhile; endif;?>
+
+			<!-- if page is content page -->
+			<?php if (is_single()) { ?>
+				<meta property="og:url" content="<?php the_permalink() ?>"/>
+				<meta property="og:title" content="<?php single_post_title(''); ?>" />
+				<meta property="og:description" content="<?php echo strip_tags(get_the_excerpt($post->ID)); ?>" />
+				<meta property="og:type" content="article" />
+				<meta property="og:image" content="<?php if (function_exists('wp_get_attachment_thumb_url')) {echo wp_get_attachment_thumb_url(get_post_thumbnail_id($post->ID)); }?>" />
+
+			<!-- if page is others -->
+			<?php } else { ?>
+				<meta property="og:site_name" content="<?php bloginfo('name'); ?>" />
+				<meta property="og:description" content="<?php bloginfo('description'); ?>" />
+				<meta property="og:type" content="website" />
+				<meta property="og:image" content="<?php bloginfo('template_directory'); ?>/images/fb-logo.png" /> <?php } ?>
+
+
+
 		<?php wp_head(); ?>
