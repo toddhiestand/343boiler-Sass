3324128eccf6eaf75e31be8d86220f9d0f920ea4
Todd Hiestand <todd@purecharity.com>
2014-04-30 12:39:15 -0500

    Remove footer menu - we never use it

--- footer.php
+++ footer.php
@@ -2,9 +2,6 @@
 			<div class="container">
 				<div class="row">
 					<div class="footer">
-						<nav id="footer">
-							<?php wp_nav_menu( array( 'theme_location' => 'utility-menu' ) ); ?>
-						</nav>
 						<p>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> | <?php $options = get_option('clean_theme_options'); echo $options['footer_address']; ?></p>
 					</div><!-- end footer -->	
 				</div><!-- end row -->
