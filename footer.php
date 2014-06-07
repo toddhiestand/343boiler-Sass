86ba37539a6999bd79ec978441f0070aa923ffd8
Todd Hiestand <todd@purecharity.com>
2014-06-07 16:19:07 -0400

    Remove old code from footer

---
+++ footer.php
@@ -0,0 +1,12 @@
+		<div class="wide-container footer-wide">
+			<div class="container">
+				<div class="row">
+					<div class="footer">
+						<p>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></p>
+					</div><!-- end footer -->	
+				</div><!-- end row -->
+			</div><!-- end container -->
+		</div>
+	<?php wp_footer(); ?>
+	</body>
+</html>
